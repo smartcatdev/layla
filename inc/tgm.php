326e6ff11fcb1246c676ee57019aa3378eef9672
bilalhassan <info@ottawapc.cagit config --global user.email bilal@smartcat.cagit config --global user.name Bilal>
2017-04-02 09:49:06 -0400

    added ucare to the plugin list

--- inc/tgm.php
+++ inc/tgm.php
@@ -63,7 +63,12 @@ function layla_register_required_plugins() {
                 'name' => __( 'Our Team Showcase', 'layla' ),
                 'slug' => 'our-team-enhanced',
                 'required' => false,
-            )
+            ),
+            array (
+                'name' => __( 'uCare - Support Ticket System', 'layla' ),
+                'slug' => 'ucare-support-system',
+                'required' => false,
+            ),
         );
 
 	/*
